238498f06dcc4cb81607ddfe6ee4011b9ac68f36
Austin Fell <austinfell94@gmail.com>
2025-04-14 22:22:16 -0400

    MIDI Successfully loading data in from browser midi.

--- client/src/vtakt_client/midi/views.cljs
+++ client/src/vtakt_client/midi/views.cljs
@@ -5,7 +5,48 @@
    [vtakt-client.midi.styles :as styles]
    [reagent.core :as r]))
 
-(defn midi-status-indicator
+(defonce midi-outputs (r/atom {}))
+
+(defn init-midi!
+  "Initialize the Web MIDI API and store available outputs."
+  []
+  (-> (.requestMIDIAccess js/navigator)
+      (.then
+       (fn [access]
+         (reset! midi-outputs {})
+         ;; Get all available MIDI outputs
+         (let [outputs (.-outputs access)]
+           (.forEach outputs
+                     (fn [output key]
+                       (swap! midi-outputs assoc key
+                              {:id key
+                               :name (.-name output)
+                               :manufacturer (.-manufacturer output)
+                               :output output})))
+           ;; Set up listener for device changes
+           (set! (.-onstatechange access)
+                 (fn [e]
+                   (let [port (.-port e)
+                         state (.-state port)
+                         type (.-type port)]
+                     ;; Only handle output devices
+                     (when (= type "output")
+                       (if (= state "connected")
+                         ;; Add new device or update existing
+                         (swap! midi-outputs assoc (.-id port)
+                                {:id (.-id port)
+                                 :name (.-name port)
+                                 :manufacturer (.-manufacturer port)
+                                 :output port})
+                         ;; Remove disconnected device
+                         (swap! midi-outputs dissoc (.-id port)))))))))
+
+       ;; Handle errors
+       (fn [err]
+         (js/console.error "Failed to initialize MIDI:" err))))
+  nil)
+
+(defn midi-not-configured-alert
   "Component showing MIDI connection status."
   []
   (fn []
@@ -19,12 +60,21 @@
 (defn midi-configurator
   "Root level component that allows configuration of midi"
   []
-  (fn []
-    [re-com/v-box
-     :class (styles/configurator-container)
-     :gap "15px"
-     :children [[re-com/title
-                 :label "MIDI Configuration"
-                 :level :level2
-                 :class (styles/configurator-title)]
-                [midi-status-indicator]]]))
+  (r/create-class
+   {:component-did-mount
+    (fn [this]
+      (init-midi!)
+      (println @midi-outputs)
+      (println "MIDI configurator mounted"))
+
+    :reagent-render
+    (fn []
+      [re-com/v-box
+       :class (styles/configurator-container)
+       :gap "15px"
+       :children [[re-com/title
+                   :label "MIDI Configuration"
+                   :level :level2
+                   :class (styles/configurator-title)]
+                  (when (empty? @midi-outputs)
+                    [midi-not-configured-alert])]])}))
