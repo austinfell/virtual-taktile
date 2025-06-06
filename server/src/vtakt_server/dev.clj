69a87a297158455cf95c32ae7df2404c9b1f754b
Austin Fell <austinfell94@gmail.com>
2025-06-06 00:26:28 -0400

    CORE Added the dev script to the server

---
+++ server/src/vtakt_server/dev.clj
@@ -0,0 +1,13 @@
+(ns vtakt-server.dev
+  (:require [vtakt-server.core :as v]))
+
+(defn restart []
+  (v/stop-server!)
+  (v/start-server! 8002))
+
+(defn reset-db []
+  (reset! v/db-conn nil)
+  (restart))
+
+(restart)
+
