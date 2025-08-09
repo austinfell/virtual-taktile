49439fa8e6517b26deea0e48ce8a7353948131f2
Austin Fell <austinfell94@gmail.com>
2025-08-09 01:08:42 -0400

    SEQUENCER Added discovery to the grpc server

--- sequence-player/build.rs
+++ sequence-player/build.rs
@@ -1,6 +1,18 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     tonic_build::configure()
-        .file_descriptor_set_path("src/sequence_descriptor.bin")
-        .compile(&["proto/sequence.proto"], &["proto/"])?;
+        .file_descriptor_set_path(out_dir.join("sequence_descriptor.bin"))
+        .compile(&["proto/sequence.proto"], &["proto"])?;
+
+    // Print the path for debugging
+    println!(
+        "cargo:warning=Generated descriptor at: {:?}",
+        out_dir.join("sequence_descriptor.bin")
+    );
+
     Ok(())
 }
