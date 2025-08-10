8332d797b147a86e6607194705162cf4532e0c8e
Austin Fell <austinfell94@gmail.com>
2025-08-09 23:52:24 -0400

    SEQUENCER Modularizing repository

--- sequence-player/src/main.rs
+++ sequence-player/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+mod types;
+mod server;
+
+use server::{MySequencerService, SequencerServiceServer, FILE_DESCRIPTOR_SET};
+use tonic::transport::Server;
+use tonic_reflection::server::Builder;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let sequencer_service = MySequencerService::new();
+
+    // Start the playback loop
+    sequencer_service.start_playback_loop();
+
+    println!("🎛️  Sequencer service listening on {}", addr);
+    println!("📡 gRPC reflection enabled - you can use grpcurl to explore the API");
+
+    // Build the reflection service
+    let reflection_service = Builder::configure()
+        .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+
+    Server::builder()
+        .add_service(SequencerServiceServer::new(sequencer_service))
+        .add_service(reflection_service)
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
--- sequence-player/src/server.rs
+++ sequence-player/src/server.rs
@@ -1,194 +1,20 @@
-use sequence::sequencer_service_server::{SequencerService, SequencerServiceServer};
+use sequence::sequencer_service_server::{SequencerService};
 use sequence::{CueResponse, Empty, Sequence};
-use std::fmt;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use tokio::time::interval;
-use tonic::{Request, Response, Status, transport::Server};
-use tonic_reflection::server::Builder;
+use tonic::{Request, Response, Status};
+
+// Import our extracted types
+use crate::types::{SequenceData, Trig};
 
 pub mod sequence {
     tonic::include_proto!("sequence");
 }
 
-// Try to include the file descriptor set - this should work with the fixed build.rs
-const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("sequence_descriptor");
-
-// Native Rust data structures
-#[derive(Debug, Clone, PartialEq)]
-pub struct Subdivision {
-    pub numerator: i64,
-    pub denominator: i64,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum NoteValue {
-    UnknownNote,
-    C,
-    CSharp,
-    D,
-    DSharp,
-    E,
-    F,
-    FSharp,
-    G,
-    GSharp,
-    A,
-    ASharp,
-    B,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Note {
-    pub octave: i32,
-    pub value: NoteValue,
-    pub velocity: u32,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Trig {
-    pub note: Option<Note>,
-    pub track: u32,
-    pub step: u32,
-    pub offset: f32,
-    pub length: f32,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct SequenceData {
-    pub sequence_length: u32,
-    pub trig_subdivision: Option<Subdivision>,
-    pub trigs: Vec<Trig>,
-}
-
-// Display implementations
-impl fmt::Display for Subdivision {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}/{}", self.numerator, self.denominator)
-    }
-}
-
-impl fmt::Display for NoteValue {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let note_str = match self {
-            NoteValue::UnknownNote => "?",
-            NoteValue::C => "C",
-            NoteValue::CSharp => "C#",
-            NoteValue::D => "D",
-            NoteValue::DSharp => "D#",
-            NoteValue::E => "E",
-            NoteValue::F => "F",
-            NoteValue::FSharp => "F#",
-            NoteValue::G => "G",
-            NoteValue::GSharp => "G#",
-            NoteValue::A => "A",
-            NoteValue::ASharp => "A#",
-            NoteValue::B => "B",
-        };
-        write!(f, "{}", note_str)
-    }
-}
-
-impl fmt::Display for Note {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}{} (vel: {})", self.value, self.octave, self.velocity)
-    }
-}
-
-impl fmt::Display for Trig {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let note_str = match &self.note {
-            Some(note) => format!("{}", note),
-            None => "REST".to_string(),
-        };
-        write!(
-            f,
-            "Track {}, Step {}: {} [offset: {:.2}, length: {:.2}]",
-            self.track, self.step, note_str, self.offset, self.length
-        )
-    }
-}
-
-impl fmt::Display for SequenceData {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "Sequence (length: {})", self.sequence_length)?;
-
-        if let Some(subdivision) = &self.trig_subdivision {
-            writeln!(f, "  Subdivision: {}", subdivision)?;
-        }
-
-        writeln!(f, "  Trigs ({}):", self.trigs.len())?;
-        for (i, trig) in self.trigs.iter().enumerate() {
-            writeln!(f, "    {}: {}", i + 1, trig)?;
-        }
-
-        Ok(())
-    }
-}
-
-// Conversion from protobuf to native structs
-impl From<sequence::NoteValue> for NoteValue {
-    fn from(proto_value: sequence::NoteValue) -> Self {
-        match proto_value {
-            sequence::NoteValue::UnknownNote => NoteValue::UnknownNote,
-            sequence::NoteValue::C => NoteValue::C,
-            sequence::NoteValue::CSharp => NoteValue::CSharp,
-            sequence::NoteValue::D => NoteValue::D,
-            sequence::NoteValue::DSharp => NoteValue::DSharp,
-            sequence::NoteValue::E => NoteValue::E,
-            sequence::NoteValue::F => NoteValue::F,
-            sequence::NoteValue::FSharp => NoteValue::FSharp,
-            sequence::NoteValue::G => NoteValue::G,
-            sequence::NoteValue::GSharp => NoteValue::GSharp,
-            sequence::NoteValue::A => NoteValue::A,
-            sequence::NoteValue::ASharp => NoteValue::ASharp,
-            sequence::NoteValue::B => NoteValue::B,
-        }
-    }
-}
-
-impl From<sequence::Subdivision> for Subdivision {
-    fn from(proto_subdivision: sequence::Subdivision) -> Self {
-        Subdivision {
-            numerator: proto_subdivision.numerator,
-            denominator: proto_subdivision.denominator,
-        }
-    }
-}
-
-impl From<sequence::Note> for Note {
-    fn from(proto_note: sequence::Note) -> Self {
-        Note {
-            octave: proto_note.octave,
-            value: sequence::NoteValue::try_from(proto_note.value)
-                .unwrap_or(sequence::NoteValue::UnknownNote)
-                .into(),
-            velocity: proto_note.velocity,
-        }
-    }
-}
-
-impl From<sequence::Trig> for Trig {
-    fn from(proto_trig: sequence::Trig) -> Self {
-        Trig {
-            note: proto_trig.note.map(|n| n.into()),
-            track: proto_trig.track,
-            step: proto_trig.step,
-            offset: proto_trig.offset,
-            length: proto_trig.length,
-        }
-    }
-}
-
-impl From<Sequence> for SequenceData {
-    fn from(proto_sequence: Sequence) -> Self {
-        SequenceData {
-            sequence_length: proto_sequence.sequence_length,
-            trig_subdivision: proto_sequence.trig_subdivision.map(|s| s.into()),
-            trigs: proto_sequence.trigs.into_iter().map(|t| t.into()).collect(),
-        }
-    }
-}
+// Make these public so main.rs can use them
+pub use sequence::sequencer_service_server::SequencerServiceServer;
+pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("sequence_descriptor");
 
 #[derive(Debug)]
 pub struct MySequencerService {
@@ -279,10 +105,7 @@ struct SequencerState {
 
 #[tonic::async_trait]
 impl SequencerService for MySequencerService {
-    async fn swap_sequence(
-        &self,
-        request: Request<Sequence>,
-    ) -> Result<Response<Empty>, Status> {
+    async fn swap_sequence(&self, request: Request<Sequence>) -> Result<Response<Empty>, Status> {
         println!("Got a SwapSequence request");
 
         let sequence_data: SequenceData = request.into_inner().into();
@@ -328,10 +151,10 @@ impl SequencerService for MySequencerService {
             println!("Cued new sequence");
         }
 
-        // Return a mock response
         Ok(Response::new(CueResponse {
             success: true,
-            remaining_steps: 16, // Mock value
+            // We need to rework the way our IO subsystem works to allow messaging of where it is at here.
+            remaining_steps: 16,
         }))
     }
 
@@ -360,30 +183,4 @@ impl SequencerService for MySequencerService {
 
         Ok(Response::new(Empty {}))
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let sequencer_service = MySequencerService::new();
-
-    // Start the playback loop
-    sequencer_service.start_playback_loop();
-
-    println!("🎛️  Sequencer service listening on {}", addr);
-    println!("📡 gRPC reflection enabled - you can use grpcurl to explore the API");
-
-    // Build the reflection service
-    let reflection_service = Builder::configure()
-        .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
-        .build()
-        .unwrap();
-
-    Server::builder()
-        .add_service(SequencerServiceServer::new(sequencer_service))
-        .add_service(reflection_service)
-        .serve(addr)
-        .await?;
-
-    Ok(())
 }
