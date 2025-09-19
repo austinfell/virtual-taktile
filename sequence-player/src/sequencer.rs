efc4d6411a0851f9439ffdf878586b56b4342918
Austin Fell <austinfell94@gmail.com>
2025-09-18 20:53:17 -0400

    SEQUENCER Created new event ring data structure.

--- sequence-player/src/sequencer.rs
+++ sequence-player/src/sequencer.rs
@@ -1,20 +1,12 @@
 use crate::server::sequence::Note as SequenceNote;
 use crate::server::sequence::{Sequence, Trig};
-use crate::types::sequence;
 use midir::MidiOutputConnection;
 use spin_sleep::LoopHelper;
-use wmidi::Note;
 use std::sync::mpsc::Receiver;
 use std::sync::{mpsc, Arc, Mutex};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::marker::PhantomData;
 use std::thread;
-
-// General sequencer data structure definition.
-pub trait StepHandler: Send + Sync + 'static {
-    fn handle_notes_on(&self, trigs: Vec<&Trig>);
-    fn handle_notes_off(&self, trigs: Vec<&Trig>);
-}
 
 #[derive(Debug)]
 enum Event {
@@ -22,51 +14,67 @@ enum Event {
     NoteOff(u8)
 }
 
+type Events = Vec<(Event, usize)>;
+
+struct EventRing {
+    // TODO - We'll want to make this a swappable pair so that once we get to a position, if the
+    // other events buffer is populated, it clears out the old one and switches to the new one...
+    events: Events,
+    position: usize,
+    tick_length: usize
+}
+
 fn parse_note_to_midi(note: &SequenceNote) -> u8 {
     ((note.octave * 12) + note.value as i32).try_into().unwrap()
 }
 
-fn sequence_to_sequencer_state(sequence: &Sequence) -> SequencerState {
-    let mut events = Vec::new();
-
-    let sequence_length_ticks = sequence.sequence_length * 256;
-
-    for trig in &sequence.trigs {
-        if let Some(note) = &trig.note {
-            let midi_pitch = parse_note_to_midi(note);
-            let note_on_tick = (trig.step * 256) % sequence_length_ticks;
-            events.push((Event::NoteOn(midi_pitch), note_on_tick));
-            let note_off_tick = (note_on_tick + 64) % sequence_length_ticks;
-            events.push((Event::NoteOff(midi_pitch), note_off_tick));
-        }
-    }
-
-    events.sort_by_key(|(_, tick)| *tick);
-
-    SequencerState {
-        current_sequence: events,
-        sequence_length: sequence_length_ticks,
-    }
-}
-
-
-#[derive(Debug, Default)]
-struct SequencerState {
-    current_sequence: Vec<(Event, u32)>,
-    sequence_length: u32
-}
-
-impl SequencerState {
+impl EventRing {
     fn new() -> Self {
         Self {
-            current_sequence: Vec::new(),
-            sequence_length: 256 * 16
+            events: Vec::with_capacity(0),
+            position: 0,
+            tick_length: 0
         }
     }
 
-    fn swap_sequence(&mut self, new_sequence: Sequence) {
-        *self = sequence_to_sequencer_state(&new_sequence);
-        println!("{:?}", self.current_sequence);
+    fn swap_sequence(&mut self, sequence: &Sequence) {
+        let mut events = Vec::with_capacity(sequence.trigs.len() * 2);
+
+        let sequence_length_ticks = (sequence.sequence_length * 256) as usize;
+
+        for trig in &sequence.trigs {
+            if let Some(note) = &trig.note {
+                let midi_pitch = parse_note_to_midi(note);
+                let note_on_tick = (trig.step * 256) as usize % sequence_length_ticks;
+                events.push((Event::NoteOn(midi_pitch), note_on_tick));
+                let note_off_tick = (note_on_tick + 64) as usize % sequence_length_ticks;
+                events.push((Event::NoteOff(midi_pitch), note_off_tick));
+            }
+        }
+
+        events.sort_by_key(|(_, tick)| *tick);
+
+        self.events = events;
+        self.position = 0;
+        self.tick_length = sequence_length_ticks;
+    }
+
+    fn peek(&self) -> Option<&(Event, usize)> {
+        if self.events.is_empty() {
+            return None;
+        }
+
+        Some(&self.events[self.position])
+    }
+
+    fn take(&mut self) -> Option<&(Event, usize)> {
+        if self.events.is_empty() {
+            return None;
+        }
+
+        let r = Some(&self.events[self.position]);
+        self.position = (self.position + 1) % self.events.len();
+        r
     }
 }
 
@@ -132,10 +140,16 @@ pub trait Sequencer : Send + Sync + 'static {
     fn cue_sequence(&mut self, s: Sequence) -> CueResult;
 }
 
+// General sequencer data structure definition.
+pub trait StepHandler: Send + Sync + 'static {
+    fn handle_notes_on(&self, trigs: Vec<&Trig>);
+    fn handle_notes_off(&self, trigs: Vec<&Trig>);
+}
+
 pub struct CoreSequencer<T: StepHandler> {
     running: Arc<AtomicBool>,
     bpm_tx: mpsc::Sender<f32>,
-    sequencer_state: Arc<Mutex<SequencerState>>,
+    event_ring: Arc<Mutex<EventRing>>,
     _phantom: std::marker::PhantomData<T>
 }
 
@@ -144,30 +158,33 @@ impl<T: StepHandler> CoreSequencer<T> {
         let running = Arc::new(AtomicBool::new(false));
         let (bpm_tx, bpm_rx) = mpsc::channel();
 
-        let sequencer_state = Arc::new(Mutex::new(SequencerState::new()));
-        let sequencer_state_clone = sequencer_state.clone();
+        let event_ring = Arc::new(Mutex::new(EventRing::new()));
+        let event_ring_clone = event_ring.clone();
 
         let running_clone = Arc::clone(&running);
         thread::spawn(move || {
-            sequencer_loop(running_clone, bpm_rx, sequencer_state_clone, step_handler);
+            sequencer_loop(running_clone, bpm_rx, event_ring_clone, step_handler);
         });
 
         CoreSequencer {
             running,
             bpm_tx,
-            sequencer_state,
+            event_ring,
             _phantom: PhantomData
         }
     }
 
 }
 
-fn sequencer_loop<T: StepHandler>(running: Arc<AtomicBool>, bpm_rx: Receiver<f32>, seq: Arc<Mutex<SequencerState>>, step_handler: T) {
+fn sequencer_loop<T: StepHandler>(running: Arc<AtomicBool>, bpm_rx: Receiver<f32>, seq: Arc<Mutex<EventRing>>, step_handler: T) {
     let mut loop_helper = LoopHelper::builder()
         .build_with_target_rate(1000.0);
     let mut tick = 0;
+
     loop {
         loop_helper.loop_start();
+
+        // Handle BPM changes
         match bpm_rx.try_recv() {
             Ok(bpm) => {
                 let tps = (bpm * 256.0) / 60.0;
@@ -178,76 +195,82 @@ fn sequencer_loop<T: StepHandler>(running: Arc<AtomicBool>, bpm_rx: Receiver<f32
                 // TODO - implement error handling.
             }
         }
-        if running.load(Ordering::Relaxed) {
-            // TODO - Tick needs to be more precisely controlled to handle jitter.
 
+        if running.load(Ordering::Relaxed) {
             // Collect notes on and off events for this tick
             let mut notes_on = Vec::new();
             let mut notes_off = Vec::new();
 
             {
-                let state = seq.lock().unwrap();
-                for (event, event_tick) in &state.current_sequence {
+                let mut event_ring = seq.lock().unwrap();
+
+                // Process events that match the current tick
+                // We need to check if we have events and if the next event is for this tick
+                while let Some((event, event_tick)) = event_ring.peek() {
                     if *event_tick == tick {
-                        match event {
-                            Event::NoteOn(pitch) => {
-                                let trig = Trig {
-                                    note: Some(SequenceNote {
-                                        octave: (*pitch as i32 / 12) - 1, // Convert MIDI pitch back to octave
-                                        value: (*pitch as i32) % 12,      // Convert MIDI pitch back to note value
-                                        velocity: 100, // Default velocity, you might want to store this
-                                    }),
-                                    track: 0,    // Default track, you might want to store this
-                                    step: 0,     // Could calculate from tick if needed
-                                    offset: 0,   // Could calculate from tick if needed
-                                    length: None, // Not relevant for note on
-                                };
-                                notes_on.push(trig);
-                            }
-                            Event::NoteOff(pitch) => {
-                                // Create a minimal Trig for the note off event
-                                let trig = Trig {
-                                    note: Some(SequenceNote {
-                                        octave: (*pitch as i32 / 12) - 1,
-                                        value: (*pitch as i32) % 12,
-                                        velocity: 0, // Note off typically has 0 velocity
-                                    }),
-                                    track: 0,
-                                    step: 0,
-                                    offset: 0,
-                                    length: None,
-                                };
-                                notes_off.push(trig);
+                        // This event is for the current tick - take it and process
+                        if let Some((event, _)) = event_ring.take() {
+                            match event {
+                                Event::NoteOn(pitch) => {
+                                    let trig = Trig {
+                                        note: Some(SequenceNote {
+                                            octave: (*pitch as i32 / 12) - 1,
+                                            value: (*pitch as i32) % 12,
+                                            velocity: 100,
+                                        }),
+                                        track: 0,
+                                        step: 0,
+                                        offset: 0,
+                                        length: None,
+                                    };
+                                    notes_on.push(trig);
+                                }
+                                Event::NoteOff(pitch) => {
+                                    let trig = Trig {
+                                        note: Some(SequenceNote {
+                                            octave: (*pitch as i32 / 12) - 1,
+                                            value: (*pitch as i32) % 12,
+                                            velocity: 0,
+                                        }),
+                                        track: 0,
+                                        step: 0,
+                                        offset: 0,
+                                        length: None,
+                                    };
+                                    notes_off.push(trig);
+                                }
                             }
                         }
+                    } else {
+                        // Next event is not for this tick, stop processing
+                        break;
                     }
                 }
             }
-            
+
             // Handle the collected events
             if !notes_on.is_empty() {
                 let trig_refs: Vec<&Trig> = notes_on.iter().collect();
                 step_handler.handle_notes_on(trig_refs);
             }
-            
+
             if !notes_off.is_empty() {
                 let trig_refs: Vec<&Trig> = notes_off.iter().collect();
                 step_handler.handle_notes_off(trig_refs);
             }
-            
+
             tick += 1;
+
             // Handle sequence looping
             {
-                let state = seq.lock().unwrap();
-                if tick >= state.sequence_length {
+                let event_ring = seq.lock().unwrap();
+                if tick >= event_ring.tick_length {
                     tick = 0;
                     println!("Sequence loop - back to tick 0");
                 }
             }
         }
-        // TODO - Fetch current notes.
-        // Interface &[usize...] => &[Trig...]
-        // TODO - Use handle notes on to play notes.
+
         loop_helper.loop_sleep();
     }
 }
@@ -266,13 +289,13 @@ impl<T: StepHandler> Sequencer for CoreSequencer<T> {
 
     fn swap_sequence(&mut self, s: Sequence) -> SwapResult {
         self.bpm_tx.send(s.bpm);
-        self.sequencer_state.lock().unwrap().swap_sequence(s);
+        self.event_ring.lock().unwrap().swap_sequence(&s);
         Result::Ok(SwapMetadata { replaced_existing: true })
     }
 
     fn cue_sequence(&mut self, s: Sequence) -> CueResult {
         self.bpm_tx.send(s.bpm);
-        self.sequencer_state.lock().unwrap().swap_sequence(s);
+        self.event_ring.lock().unwrap().swap_sequence(&s);
         Result::Ok(CueMetadata { replaced_existing: true, remaining_steps: 0 })
     }
 }
