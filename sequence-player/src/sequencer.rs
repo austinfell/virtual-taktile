66dd5e5477033637d897d6b269d214a248ace4d1
Austin Fell <austinfell94@gmail.com>
2025-09-25 02:06:15 -0400

    SEQUENCER Reimplemented sequencer to use purely borrows for playback. No more clones.

--- sequence-player/src/sequencer.rs
+++ sequence-player/src/sequencer.rs
@@ -41,8 +41,18 @@ impl EventBuffer {
         self.events.len()
     }
 
-    fn get(&self, index: usize) -> Option<&(Event, usize)> {
-        self.events.get(index)
+    fn get_events_matching_tick(&self, start_index: usize) -> Option<&[(Event, usize)]> {
+        let Some(start_el) = self.events.get(start_index) else {
+            return None
+        };
+
+        let mut end_index = start_index + 1;
+
+        while start_index != end_index && start_el.1 == self.events[end_index % self.events.len()].1 {
+            end_index += 1
+        }
+
+        Some(&self.events[start_index..end_index])
     }
 }
 
@@ -77,6 +87,7 @@ impl EventRing {
                 let midi_pitch = parse_note_to_midi(note);
                 let note_on_tick = (trig.step * 256) as usize % sequence_length_ticks;
                 events.push((Event::NoteOn(midi_pitch, note.velocity as u8, trig.track as u8), note_on_tick));
+                // TODO Need to use note length instead...
                 let note_off_tick = (note_on_tick + 64) as usize % sequence_length_ticks;
                 events.push((Event::NoteOff(midi_pitch, note.velocity as u8, trig.track as u8), note_off_tick));
             }
@@ -107,27 +118,26 @@ impl EventRing {
         }
     }
 
-    fn peek(&mut self) -> Option<&(Event, usize)> {
+    fn next(&mut self) -> Option<&'_[(Event, usize)]> {
+        // Make sure we are at the correct internal buffer (This is a swappable
+        // circular queue)
         self.switch_to_cued_buffer();
 
-        let current_buffer = self.current_buffer_ref();
+        //  Get the current buffer.
+        let current_buffer = &self.buffers[self.current_buffer];
         if current_buffer.is_empty() {
             return None;
         }
 
-        current_buffer.get(self.position)
-    }
-
-    fn take(&mut self) -> Option<&(Event, usize)> {
-        if self.current_buffer_ref().is_empty() {
+        // Get all events at the current position.
+        let Some(events) = current_buffer.get_events_matching_tick(self.position) else {
             return None;
-        }
+        };
 
-        let current_pos = self.position;
-        let buffer_len = self.current_buffer_ref().len();
-        self.position = (self.position + 1) % buffer_len;
+        // Increment to the next position.
+        self.position = (self.position + (events.len())) % current_buffer.len();
 
-        self.current_buffer_ref().get(current_pos)
+        Some(events)
     }
 
     fn tick_len(&self) -> usize {
@@ -203,7 +213,7 @@ pub trait Sequencer : Send + Sync + 'static {
 
 // General sequencer data structure definition.
 pub trait StepHandler: Send + Sync + 'static {
-    fn handle_events(&self, trigs: Vec<Event, 100>);
+    fn handle_events(&self, trigs: &[(Event, usize)]);
 }
 
 pub struct CoreSequencer<T: StepHandler> {
@@ -233,54 +243,35 @@ impl<T: StepHandler> CoreSequencer<T> {
 }
 
 fn sequencer_loop<T: StepHandler>(running: Arc<AtomicBool>, seq: Arc<Mutex<EventRing>>, step_handler: T) {
-    let mut curr_bpm = 120.0;
+    let mut curr_bpm = 600.0;
     let mut tick = 0;
     let mut loop_helper = LoopHelper::builder()
         .build_with_target_rate((curr_bpm * 256.0) / 60.0);
 
+
     loop {
-        loop_helper.loop_start();
 
         if running.load(Ordering::Relaxed) {
-            // Collect notes on and off events for this tick
-            let mut events : Vec<Event, 100> = Vec::new();
+            // Implementation goes here.
+            let mut event_ring = seq.lock().unwrap();
 
-            {
-                let mut event_ring = seq.lock().unwrap();
+            let tick_len = event_ring.tick_len();
 
-                // Process events that match the current tick
-                // We need to check if we have events and if the next event is for this tick
-                while let Some((event, event_tick)) = event_ring.peek() {
-                    if *event_tick == tick {
-                        if (curr_bpm != event_ring.current_bpm()) {
-                            curr_bpm = event_ring.current_bpm();
-                            loop_helper.set_target_rate((curr_bpm * 256.0) / 60.0);
-                        }
-                        if let Some((event, _)) = event_ring.take() {
-                            events.push(event.clone());
-                        }
-                    } else {
-                        break;
-                    }
+            let next_events = event_ring.next();
+
+            while next_events.is_some() {
+                loop_helper.loop_start();
+                tick = (tick + 1) % (tick_len + 1);
+
+                if tick == next_events.unwrap().first().unwrap().1 {
+                    step_handler.handle_events(next_events.unwrap());
+                    println!("{:?}", next_events);
+                    break;
                 }
-            }
-
-            if !events.is_empty() {
-                step_handler.handle_events(events);
-            }
-
-            tick += 1;
-
-            {
-                let event_ring = seq.lock().unwrap();
-                if tick >= event_ring.tick_len() {
-                    tick = 0;
-                    println!("Loop");
-                }
+                loop_helper.loop_sleep();
             }
         }
 
-        loop_helper.loop_sleep();
     }
 }
 
@@ -320,7 +311,7 @@ impl MidiStepHandler {
 }
 
 impl StepHandler for MidiStepHandler {
-    fn handle_events(&self, events: Vec<Event, 100>) {
+    fn handle_events(&self, events: &[(Event, usize)]) {
         if events.is_empty() {
             return;
         }
@@ -328,7 +319,7 @@ impl StepHandler for MidiStepHandler {
         let mut connection = self.midi_connection.lock().unwrap();
 
         for event in events {
-            let (status_byte, note, velocity, channel, event_name) = match event {
+            let (status_byte, note, velocity, channel, event_name) = match event.0 {
                 Event::NoteOff(note, velocity, channel) => {
                     (0x80, note, velocity, channel, "NoteOff")
                 },
