a2612189352197aa4ea875f4e4b3181d56be56b3
Austin Fell <austinfell94@gmail.com>
2025-10-06 01:14:18 -0400

    SEQUENCER rough implementation of offsets

--- sequence-player/src/sequencer.rs
+++ sequence-player/src/sequencer.rs
@@ -93,10 +93,10 @@ impl EventRing {
         for trig in &sequence.trigs {
             if let Some(note) = &trig.note {
                 let midi_pitch = parse_note_to_midi(note);
-                let note_on_tick = (trig.step * 256) as usize % sequence_length_ticks;
+                let note_on_tick = ((trig.step as i32 * 256) + trig.offset).rem_euclid(sequence_length_ticks.try_into().unwrap()) as usize;
                 events.push((Event::NoteOn(midi_pitch, note.velocity as u8, trig.track as u8), note_on_tick));
                 // TODO Need to use note length instead...
-                let note_off_tick = (note_on_tick + 64) as usize % sequence_length_ticks;
+                let note_off_tick = ((trig.step as i32 * 256) + trig.offset + 64).rem_euclid(sequence_length_ticks.try_into().unwrap()) as usize;
                 events.push((Event::NoteOff(midi_pitch, note.velocity as u8, trig.track as u8), note_off_tick));
             }
         }
